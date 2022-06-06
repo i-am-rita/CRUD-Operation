6ea2fd0b0039f1143225b5a8400e8cb4e6517c94
Rita Uzoma <you@example.com>
2022-06-06 10:34:57 +0100

    complete

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -7,22 +7,55 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Update = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [ID, setID] = useState(null);
+  // const [state, setState] = useState({
+  //     id: "",
+  //     firstName: "",
+  //     lastName: "",
+  // })
+  const [firstName, setFirstName] = useState(
+    localStorage.getItem("First Name")
+  );
+  const [lastName, setLastName] = useState(localStorage.getItem("Last Name"));
+  const [ID, setID] = useState(localStorage.getItem("ID"));
   console.log(ID);
 
+  //   const returnData = () => {
+  //       axios.put(`https://6295e8a1810c00c1cb6afc7d.mockapi.io/crudData/${ID}`, {
+  //           firstName,
+  //           lastName
+  //       })
+  //   }
+
   const returnData = () => {
+    localStorage.setItem("ID", ID);
+    localStorage.setItem("First Name", firstName);
+    localStorage.setItem("Last Name", lastName);
     axios.put(`https://6295e8a1810c00c1cb6afc7d.mockapi.io/crudData/${ID}`, {
       firstName,
-      lastName
+      lastName,
     });
+    // localStorage.setItem("Checkbox", checkbox);
+    // console.log(id);
   };
-  useEffect(() => {
-    setFirstName(localStorage.getItem("First Name"));
-    setLastName(localStorage.getItem("Last Name"));
-    setID(localStorage.getItem("ID"));
-  });
+
+  //   const returnData = (ID, firstName, lastName) => {
+  //     axios.put(`https://6295e8a1810c00c1cb6afc7d.mockapi.io/crudData/${ID}`, {
+  // firstName,
+  //     });
+  //   .then(() => {
+  // localStorage.setItem("ID", ID),
+  //   localStorage.setItem("First Name", firstName),
+  //   localStorage.setItem("Last Name", lastName);
+  //   });
+  //   };
+
+  //   const handleChange = (e) => {
+  //       setState(prevState => {
+  //           ...prevState,
+  //           [e.target.name]: e.target.value,
+  //       });
+  // console.log("changed");
+  //   };
   return (
     <>
       <div className="create-form">
@@ -45,6 +78,7 @@ const Update = () => {
               value={lastName}
               //   placeholder="Last Name"
               onChange={(e) => setLastName(e.target.value)}
+              // onChange={handleChange}
             />
           </form>
           {/* <Checkbox
